9b5c94f2bdc15d6a15faa51842076e6af749a5e9
Seolhayoung <gkdud4930@naver.com>
2024-12-20 22:04:37 +0900

    5

---
+++ 5/base_code/cal_diets.c
@@ -0,0 +1,87 @@
+//
+//  cal_diets.c
+//  Diets for Calorie Diary
+//
+//  Created by Eunju Cha
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cal_exercise.h"
+#include "cal_diets.h"
+#include "cal_healthdata.h"
+
+#define MAX_DIETS 100      			// Maximum number of diets
+#define MAX_FOOD_NAME_LEN 50		// Maximum length of the name of food
+
+
+// list of diets 
+static Diet diet_list[MAX_DIETS];
+static int diet_list_size = 0;
+
+
+/*
+    description : read the information in "diets.txt"
+*/
+
+void loadDiets(const char* DIETFILEPATH) {
+    FILE *file = fopen(DIETFILEPATH, "r");
+    if (file == NULL) {
+        printf("There is no file for diets! \n");
+    }
+
+     // ToCode: to read a list of the diets from the given file
+    while (fscanf(file,"%s %d",diet_list[diet_list_size].food_name,&diet_list[diet_list_size].calories_intake)==2) {
+    	// Read line by line from the file //If you have successfully read two values from a file, increase the diet_list_size
+    	diet_list_size++;
+        if (diet_list_size >= MAX_DIETS){
+        	break; // If exercise list reach MAX_DIETS, Exit the while loop.
+		}
+    }
+    fclose(file); // fopen and fclose are a pair
+}
+
+/*
+    description : to enter the selected diet and the total calories intake in the health data
+    input parameters : health_data - data object to which the selected diet and its calories are added 
+    return value : No
+    
+    operation : 1. provide the options for the diets to be selected
+    			2. enter the selected diet and the total calories intake in the health data
+*/
+
+void inputDiet(HealthData* health_data) {
+    int choice, i;
+    
+    // ToCode: to provide the options for the diets to be selected
+    printf("The list of diets:\n");
+    for(i=0;i<diet_list_size;i++){
+    	printf("%d. %s (%d kcal)\n",i+1,diet_list[i].food_name,diet_list[i].calories_intake);
+	} // print the list of diets
+    
+    printf("7. Exit\n");
+    
+	// ToCode: to enter the diet to be chosen with exit option
+	printf("Select diet: ");
+    scanf("%d", &choice); // user selects
+    if(choice==7){
+    	printf("Exit from Diet options.\n");
+	}
+	
+	if(choice<0||choice>diet_list_size+1){
+		printf("[Error] Invalid option.\n");
+	}
+
+    // ToCode: to enter the selected diet in the health data
+    int calories_intake=diet_list[choice-1].calories_intake; //
+
+    // ToCode: to enter the total calories intake in the health data
+	strcpy(health_data->diet[health_data->diet_count].food_name, diet_list[choice - 1].food_name);
+    health_data->diet[health_data->diet_count].calories_intake = calories_intake;
+    health_data->diet_count++;
+    health_data->total_calories_intake += calories_intake;
+
+}
+
---
+++ 5/base_code/cal_exercise.c
@@ -0,0 +1,99 @@
+//
+//  cal_exercise.c
+//  Excercise for Calorie Diary
+//
+//  Created by Eunju Cha
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cal_exercise.h"
+#include "cal_diets.h"
+#include "cal_healthdata.h"
+
+#define MAX_EXERCISES 100  			// Maximum number of exercises
+#define MAX_EXERCISE_NAME_LEN 50	// Maximum length of the name of exercise
+
+
+// To declare the structure of the exercises
+static Exercise exercise_list[MAX_EXERCISES];
+int exercise_list_size = 0;
+
+
+/*
+    description : read the information in "excercises.txt"
+*/
+
+void loadExercises(const char* EXERCISEFILEPATH) {
+    FILE *file = fopen(EXERCISEFILEPATH, "r"); // reading mode
+    if (file == NULL) {
+        printf("There is no file for exercises! \n");
+    }
+
+    // ToCode: to read a list of the exercises from the given file
+    while (fscanf(file,"%s %d",exercise_list[exercise_list_size].exercise_name, &exercise_list[exercise_list_size].calories_burned_per_minute)==2) {
+    	exercise_list_size++; // Read line by line from the file and save it in the exercise_list array
+    	// If you have successfully read two values from a file, increase the exercise_list_size
+        if (exercise_list_size >= MAX_EXERCISES){
+        	break; // If exercise list reach MAX_EXERCISE, Exit the while loop.
+		}
+    }
+
+    fclose(file); // fopen and fclose are a pair
+}
+
+
+/*
+    description : to enter the selected exercise and the total calories burned in the health data
+    input parameters : health_data - data object to which the selected exercise and its calories are added 
+    return value : No
+    
+    operation : 1. provide the options for the exercises to be selected
+    			2. enter the duration of the exercise
+    			3. enter the selected exercise and the total calories burned in the health data
+*/
+
+void inputExercise(HealthData* health_data) {
+    int choice, duration, i;
+    
+    // ToCode: to provide the options for the exercises to be selected
+    printf("The list of exercises: \n");
+    for(i=0;i<exercise_list_size;i++){
+    	printf("%d. %s (%d kcal/min)\n",i+1, exercise_list[i].exercise_name, exercise_list[i].calories_burned_per_minute); // Numbering from 1
+	}
+	printf("7. Exit\n");
+	
+
+    // ToCode: to enter the exercise to be chosen with exit option
+	printf("Select exercise: ");
+	scanf("%d",&choice); // user selects
+	if(choice==7){
+		printf("Exit from Exercise options.\n");
+	}
+	
+	if(choice<0||choice>exercise_list_size+1){
+		printf("[Error] Invaild option.\n"); //Exit the function
+	}
+ 
+    
+    // To enter the duration of the exercise
+    printf("Enter the duration of the exercise (in min.): ");
+    scanf("%d", &duration);
+    
+    if(duration<=0){
+    	printf("[Error] Duration must be positive.\n");
+	}
+
+    // ToCode: to enter the selected exercise and total calcories burned in the health data
+    int calories_burned = exercise_list[choice-1].calories_burned_per_minute*duration; // The reason for doing -1 is that the starting index of the array is 0
+    
+    // save exercise to health_data
+    strcpy(health_data->exercises[health_data->exercise_count].exercise_name,exercise_list[choice-1].exercise_name); // copy the name of the selected exercise
+    health_data->exercises[health_data->exercise_count].calories_burned_per_minute=exercise_list[choice-1].calories_burned_per_minute; // copy the calories
+	health_data->exercise_count++; // increase in the number of exercises
+    health_data->total_calories_burned += calories_burned; // increasing total calorie burning
+    
+
+}
